0f341a08b60f784ada5bc7d562e4b84e4027f410
JonHalloran <angmar139@hotmail.com>
2018-06-14 12:42:46 -0700

    Add test for emit and add Listener

--- eventEmitter.test.js
+++ eventEmitter.test.js
@@ -1,4 +1,5 @@
 const eventEmitter = require('./eventEmitter');
+// const jest = require('jest');
 
 function callback1() {}
 
@@ -6,9 +7,53 @@ beforeEach(() => {
   const eventEmitter1 = new eventEmitter();
 });
 
-test('Can create eventEmitter', () => {
-  const eventEmitter1 = new eventEmitter();
-  expect(eventEmitter1).toBeInstanceOf(eventEmitter);
+describe('creation', () => {
+  test('Can create eventEmitter', () => {
+    const eventEmitter1 = new eventEmitter();
+    expect(eventEmitter1).toBeInstanceOf(eventEmitter);
+  });
 });
 
-test('Can add listeners to event Emitter');
+describe('emit', () => {
+  test('emit does not error on events with no listeners', () => {
+    const eventEmitter1 = new eventEmitter();
+    eventEmitter1.emit('event');
+    const emitTest = jest.fn(() => eventEmitter1.emit('event'));
+    emitTest();
+    expect(emitTest).toReturn();
+  });
+});
+
+describe('addListener', () => {
+  const eventEmitter1 = new eventEmitter();
+  const mock1 = jest.fn();
+  const mock2 = jest.fn();
+  const mock3 = jest.fn();
+  const mock4 = jest.fn();
+
+  test('Can add listener to event', () => {
+    eventEmitter1.addListener('event1', mock1);
+    eventEmitter1.emit('event1');
+    expect(mock1).toBeCalled();
+  });
+
+  test('Can add multiple listeners to different events', () => {
+    eventEmitter1.addListener('event1', mock1);
+    eventEmitter1.addListener('event2', mock2);
+    eventEmitter1.emit('event1');
+    expect(mock1).toBeCalled();
+    expect(mock2)
+      .not
+      .toBeCalled();
+    eventEmitter1.emit('event2');
+    expect(mock2).toBeCalled();
+  });
+
+  test('Can add multiple listeners to same event', () => {
+    eventEmitter1.addListener('event1', mock1);
+    eventEmitter1.addListener('event1', mock2);
+    expect(mock1).toBeCalled();
+    expect(mock2).toBeCalled();
+    eventEmitter1.emit('event1');
+  });
+});
